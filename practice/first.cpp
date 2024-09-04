0174500e9f6845034bc6ad44bd9e27ab09d48fd3
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2024-09-05 03:49:43 +0530

    updating operators

--- practice/first.cpp
+++ practice/first.cpp
@@ -27,3 +27,41 @@ using namespace std;
 
 
 
+// Taking input
+
+// int main(){
+//     double price;
+//     cout << "Enter the price:" << endl;
+//     cin >> price;
+
+//     cout << "your entered price = " << price << endl;
+//     return 0;
+// }
+
+
+// Airthmatic Operators
+
+// int main(){
+//     int a, b;
+//     cout << "Enter the value of a: " << endl;
+//     cin >> a;
+//     cout << "Enter the value of b: " << endl;
+//     cin >> b;
+
+//     cout << "sum = " << ( a + b ) << endl;
+//     cout << "Difference = " << ( a - b ) << endl;
+//     cout << "Product = " << ( a * b ) << endl;
+//     cout << "Division = " << ( a / b ) << endl;
+//     cout << "modulo = " << ( a % b ) << endl;
+//     cout << ( a / (double)b ) << endl;
+//     cout << ( a < b ) << endl;
+//     cout << ( a > b ) << endl;
+//     cout << ( a <= b ) << endl;
+//     cout << ( a >= b ) << endl;
+//     cout << ( a == b ) << endl;
+//     cout << ( a != b ) << endl;
+//     cout << !( a > b ) << endl;
+//     cout << ( ( a > b ) || ( a < b ) ) << endl;
+//     cout << ( ( a > b ) && ( a < b ) ) << endl;
+//     return 0;
+// }
