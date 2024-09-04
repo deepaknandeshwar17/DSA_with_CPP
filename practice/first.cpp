097c534fe88e918d99f4288de899c9e2c75a4b46
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2024-09-05 02:50:05 +0530

    Upadating

--- practice/first.cpp
+++ practice/first.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+    int age = 22;
+    char fullname = 'a';
+    float PI = 3.14;
+    bool issafe = true;
+    double price = 100.99;
+
+    cout << "Hello dee\nfrom ultron" << endl;
+    cout << price << endl;
     return 0;
 }
