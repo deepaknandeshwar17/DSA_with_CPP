06d78e9a9e6d64ac7dcfeacde47401e98d279576
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2024-09-05 03:01:06 +0530

    updating with typecasting

--- practice/first.cpp
+++ practice/first.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int age = 22;
-    char fullname = 'a';
-    float PI = 3.14;
-    bool issafe = true;
-    double price = 100.99;
+// int main(){
+//     int age = 22;
+//     char fullname = 'a';
+//     float PI = 3.14;
+//     bool issafe = true;
+//     double price = 100.99;
 
-    cout << "Hello dee\nfrom ultron" << endl;
-    cout << price << endl;
-    return 0;
-}
+//     cout << "Hello dee\nfrom ultron" << endl;
+//     cout << price << endl;
+//     return 0;
+// }
+
+
+
+// Type Casting
+
+// int main(){
+//     double price = 101.99;
+    
+//     int newprice = (int)price;
+//     cout << newprice << endl;
+//     return 0;
+// }
+
+
+
