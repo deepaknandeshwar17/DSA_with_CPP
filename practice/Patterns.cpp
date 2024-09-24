7fefca0a2ca9ca84dda9c80aca75df6e1e33fce5
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2024-09-24 23:04:29 +0530

    updating

--- practice/Patterns.cpp
+++ practice/Patterns.cpp
@@ -106,3 +106,20 @@ using namespace std;
 //     return 0;
 // }
 
+
+int main(){
+    int n = 5;
+    for(int i=0; i<n; i++){
+        for(int j =0; j<(n-i-1); j++){
+            cout<<" "<<" ";
+        }
+        for(int j=1; j<=(i+1); j++){
+            cout<<j<<" ";
+        }
+        for(int j=i; j>=1; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
