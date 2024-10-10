5b861b7dcd3a054ad41aeca8e9fba3b905ec5c81
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2024-10-10 12:02:16 +0530

    Updated

---
+++ practice/functions.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+// int printHello(){
+//     cout<<"Hello"<<endl;
+//     return 3;
+// }
+
+// int minOfTwo(int a, int b){
+//     if(a<=b){
+//         return a;
+//     } else{
+//         return b;
+//     }
+// }
+
+// int main() {
+//     cout<<"min = "<<minOfTwo(10,5)<<endl;
+//     return 0;
+// }
+
+
+
+// Sum of N integers
+
+// int sumN(int n){
+//     int sum = 0;
+//     for(int i=1; i<=n; i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+
+// int main(){
+//     cout<<sumN(5)<<endl;
+//     return 0;
+// }
+
+
+
+
+// Factorial of a number
+
+// int fact(int n){
+//     int factN = 1;
+//     for (int i =1; i<=n; i++){
+//         factN *= i;
+//     }
+//     return factN;
+// }
+
+// int main(){
+//     cout<<fact(5)<<endl;
+//     return 0;
+// }
+
